1fd308d8f93fa19ff782730a312bc71b85479a83
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-02-15 15:04:28 -0500

    Prject Updated

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 let characters = []; 
 
-document.addEventListener('DOMContentLoaded', () => {
+
  fetch('http://localhost:3000/characters')
    .then(data => {
      return data.json();
    })
    .then(data => {
+    
+
      characters = data;
    });
-});
+
+
 
 
 
@@ -45,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const userInput = document.querySelector('#mySearch').value              
         const narutoCharacters = characters.filter(character=>character.name.includes(userInput))
         const resultsDiv = document.querySelector('#results');
-        resultsDiv.innerHTML = ''; // Clear previous results
+        resultsDiv.innerHTML = ''; 
         narutoCharacters.forEach(character => {
             const characterDiv = document.createElement('div');
             characterDiv.innerHTML = `
@@ -53,6 +56,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 <p> Jutsu: ${character.jutsu}</p>
                 <p> Nature Type: ${character.natureType}</p>
                 <p> Unique Triats: ${character.uniqueTraits}</p>
+                
+            
                 <img src="${character.images}" alt="${character.name}">
 
             `;
@@ -60,5 +65,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
       });
       
-     
+const searchInput = document.querySelector('#mySearch');
+
+searchInput.addEventListener('input', (event) => {
+    event.preventDefault();
+    const userInput = event.target.value;
+    const narutoCharacters = characters.filter(character => character.name.includes(userInput));
+    const resultsDiv = document.querySelector('#results');
+    resultsDiv.innerHTML = ''; 
+    narutoCharacters.forEach(character => {
+        const characterDiv = document.createElement('div');
+        characterDiv.innerHTML = `
+            <h2>${character.name}</h2>
+            <p> Jutsu: ${character.jutsu}</p>
+            <p> Nature Type: ${character.natureType}</p>
+            <p> Unique Traits: ${character.uniqueTraits}</p>
+           
+            <img src="${character.images}" alt="${character.name}">
+
+        `;
+        resultsDiv.appendChild(characterDiv);
+    });
+});     
     
